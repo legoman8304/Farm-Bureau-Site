972e0e7196e6dc8ecd5ecb0e6790b7f486befde3
legoman8304 <legoman8304@gmail.com>
2019-03-12 10:44:49 -0400

    the breaks were too long

--- javascript/main.js
+++ javascript/main.js
@@ -1,13 +1,13 @@
 //top
 
-//showFallbackImage() declaration=======================
+//showFallbackImage() declaration======================
 function showFallbackImage() {
     document.getElementById("fallbackImage").className = "show";
     document.getElementById("fallbackImage1").className = "hide";
 }
-//end of showFallbackImage() declaration=================
+//end of showFallbackImage() declaration================
 
-//device detection block ================================
+//device detection block ===============================
 var isMobile = false;
 
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
@@ -20,9 +20,9 @@ console.log("Mobile device");
 else {
 console.log("Not a mobile device");
 }
-//end of device detection block==========================
-//device detection block secondary method================
+//end of device detection block=========================
+//device detection block secondary method===============
 function isMobileDevice() {
     return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
 };
-//end of device detection block secondary method=========
+//end of device detection block secondary method========
