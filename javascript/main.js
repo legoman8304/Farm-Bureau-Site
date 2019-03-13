8ed51508b7531a831b01493d7c8bad2226762338
legoman8304 <legoman8304@gmail.com>
2019-03-13 09:51:01 -0400

    Finally found a way for my adblock detetction

--- javascript/main.js
+++ javascript/main.js
@@ -1,28 +1,29 @@
 //top
 
-//showFallbackImage() declaration======================
+//showFallbackImage() declaration======================/
 function showFallbackImage() {
-    document.getElementById("fallbackImage").className = "show";
-    document.getElementById("fallbackImage1").className = "hide";
+  var x = document.getElementById("fallbackImage");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 }
-//end of showFallbackImage() declaration================
+//////////////////////////////////////////////////////////
 
-//device detection block ===============================
+//device detection block ===============================/
 var isMobile = false;
 
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
  isMobile = true
 }
-
+else {
+  isMobile = false
+}
 if (isMobile = true) {
 console.log("Mobile device");
 }
 else {
 console.log("Not a mobile device");
 }
-//end of device detection block=========================
-//device detection block secondary method===============
-function isMobileDevice() {
-    return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
-};
-//end of device detection block secondary method========
+//////////////////////////////////////////////////////////
