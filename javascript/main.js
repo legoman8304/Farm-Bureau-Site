d035d16ec2cb745cf853035428cb5977ffc388b4
legoman8304 <legoman8304@gmail.com>
2019-03-14 13:07:15 -0400

    found a way through jquery to show/hide for adblocker using it instead

--- javascript/main.js
+++ javascript/main.js
@@ -1,27 +1,5 @@
 //top
 
-//showFallbackImage() declaration======================/
-function showFallbackImage() {
-  var x = document.getElementById("fallbackImage");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
-//////////////////////////////////////////////////////////
-
-//reverseshowFallbackImage() declaration======================/
-function showFallbackImage() {
-  var x = document.getElementById("reversefallbackImage");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
-//////////////////////////////////////////////////////////
-
 //device detection block ===============================/
 var isMobile = false;
 
