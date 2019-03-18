8a2cd10d12ed33cd59b925a85ecd81dfc47ad68b
legoman8304 <legoman8304@gmail.com>
2019-03-18 11:55:05 -0400

    made all js in one file

--- javascript/main.js
+++ javascript/main.js
@@ -16,3 +16,16 @@ else {
 console.debug("Not a mobile device");
 }
 //////////////////////////////////////////////////////////
+//adblock detection block ==============================/
+if( window.canRunAds === undefined ){
+  // adblocker detected, show fallback
+  $("#adblock_off").hide();
+  $(".adblock_off").hide();
+  console.debug("adblock enabled");
+}
+else {
+  $("#adblock_on").hide();
+  $(".adblock_on").hide();
+  console.debug("adblock disabled/nonexistant");
+}
+//////////////////////////////////////////////////////////
