2fa5f1de779202c89b0d4f7454cbc9f8aa1de617
Wyatt Nulton <legoman8304@gmail.com>
2019-10-23 10:25:55 -0400

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,18 +1,1 @@
 //top
-
-//device detection block ===============================/
-var isMobile = false;
-
-if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
- isMobile = true
-}
-else {
-  isMobile = false
-}
-if (isMobile = true) {
-console.debug("Is a mobile device");
-}
-else {
-console.debug("Not a mobile device");
-}
-//////////////////////////////////////////////////////////
